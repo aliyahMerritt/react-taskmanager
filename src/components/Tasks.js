fa0b5618e29df8a525349df64f33029927619823
aliyahMerritt <103625076+aliyahMerritt@users.noreply.github.com>
2022-04-25 21:30:18 -0600

    Update Tasks.js

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -4,7 +4,9 @@ const Tasks = ({tasks, onDelete, onToggle}) => {
   return (
     <>
     {tasks.map((task, index) => (
-        <Task key={index} task={task} onDelete ={onDelete} 
+        <Task key={index} 
+        task={task} 
+        onDelete ={onDelete} 
         onToggle={onToggle}/>
     ))}
     </>
